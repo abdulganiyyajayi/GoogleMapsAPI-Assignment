f80f2bf05cc192a5388be0ce936e117a18c3059d
Abdulganiyy Ajayi <abdulganiyyajayi@gmail.com>
2016-05-20 18:13:33 -0700

    Add html file with link to Google API

---
+++ API-Consumption.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function Person(){
+	
+};
+
+Person.prototype.walk = function() {
+	return "I am walking";
+};
+
+Person.prototype.talk = fuction() {
+	return "I am talking"
+};
+
+Person.prototype.dead = function(){
+	
+};
+
+Person.prototype.setName = function(name) {
+	
+};
